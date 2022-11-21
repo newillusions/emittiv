fa85d80c9552e741e9b6a27bbd166b99bd678162
newillusions <59763487+newillusions@users.noreply.github.com>
2022-11-21 10:42:28 +0400

    Add Header Info - connanical

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,9 +5,32 @@
 </script>
 
 <svelte:head>
-	<title>emittiv sensory design - about</title>
-	<meta name="description" content="emittiv about page" />
-	<link rel="canonical" href="http://www.emittiv.com/about" />
+	<!-- title max length:  -->
+	<title>emittiv - sensory design to enhance built environments</title>
+	<!-- description max length:  -->
+	<meta
+		name="description"
+		content="lighting, video, sound and scent design making spaces more appealing, luxurious, and enticing. We bring the magic of theatre into people's every day lives."
+	/>
+	<!--  cannonical:  -->
+	<link rel="canonical" href="https://www.emittiv.com/about" />
+	<!--  robots: ”index,follow” or ”noindex,follow” -->
+	<meta name="”robots”" content="”index,follow”" />
+	<!-- og:type: article or website -->
+	<meta property="og:type" content="website" />
+	<!-- og:title length: 40-60 chs -->
+	<meta property="og:title" content="emittiv - sensory design to enhance built environments" />
+	<!-- og:description max length:  2-4 sentences-->
+	<meta
+		property="og:description"
+		content="lighting, video, sound and scent design making spaces more appealing, luxurious, and enticing. We bring the magic of theatre into people's every day lives."
+	/>
+	<!-- og:image: hero image of page -->
+	<meta property="og:image" content="https://www.emittiv.com/img/logo_strap.svg" />
+	<!-- og:url: original URL of page / story -->
+	<meta property="og:url" content="https://www.emittiv.com/about" />
+	<!-- og:site_name:  -->
+	<meta property="og:site_name" content="emittiv sensory design" />
 </svelte:head>
 
 <main>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,6 +3,35 @@
 	import TheForm from './EmailForm.svelte';
 </script>
 
+<svelte:head>
+	<!-- title max length:  -->
+	<title>emittiv - sensory design to enhance built environments</title>
+	<!-- description max length:  -->
+	<meta
+		name="description"
+		content="lighting, video, sound and scent design making spaces more appealing, luxurious, and enticing. We bring the magic of theatre into people's every day lives."
+	/>
+	<!--  cannonical:  -->
+	<link rel="canonical" href="https://www.emittiv.com/contact" />
+	<!--  robots: ”index,follow” or ”noindex,follow” -->
+	<meta name="”robots”" content="”index,follow”" />
+	<!-- og:type: article or website -->
+	<meta property="og:type" content="website" />
+	<!-- og:title length: 40-60 chs -->
+	<meta property="og:title" content="emittiv - sensory design to enhance built environments" />
+	<!-- og:description max length:  2-4 sentences-->
+	<meta
+		property="og:description"
+		content="lighting, video, sound and scent design making spaces more appealing, luxurious, and enticing. We bring the magic of theatre into people's every day lives."
+	/>
+	<!-- og:image: hero image of page -->
+	<meta property="og:image" content="https://www.emittiv.com/img/logo_strap.svg" />
+	<!-- og:url: original URL of page / story -->
+	<meta property="og:url" content="https://www.emittiv.com/contact" />
+	<!-- og:site_name:  -->
+	<meta property="og:site_name" content="emittiv sensory design" />
+</svelte:head>
+
 <main>
 	<TheHead side={'order-1'} />
 	<TheForm side={'order-1'} />
